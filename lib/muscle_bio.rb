cb144cfc37273f371c5b8386be338286ed891717
Michael Clark <clarkmu@gmail.com>
2019-06-11 14:51:39 -0400

    don't check if -in exists

--- lib/muscle_bio.rb
+++ lib/muscle_bio.rb
@@ -6,29 +6,10 @@ module MuscleBio
   	file = (/darwin/ =~ RUBY_PLATFORM) != nil ? "muscle_mac_64" :
   		(/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil ? "muscle_windows_32.exe" : "muscle_linux_64"
   	file = File.join( File.dirname(__FILE__), 'runnable/'+file)
-  	if ! self.checkInFileExists(cmd)
-  		puts "-in DOES NOT EXIST"
-  		return
-  	end
   	cmd = cmd.sub 'sudo ', ''
   	cmd = cmd.sub 'muscle', file
   	cmd = 'sudo ' + cmd
 	`#{cmd}`
-  end
-
-  def self.checkInFileExists(cmd)
-  	fileIn = ''
-  	isInParam = false
-  	cmd.split(' ').each do |p|
-  		if isInParam
-  			fileIn = p
-  			isInParam = false
-  		end
-  		if p == "-in"
-  			isInParam = true
-  		end
-  	end
-  	return File.file?(fileIn)
   end
 
 end
--- spec/muscle_bio_spec.rb
+++ spec/muscle_bio_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe MuscleBio do
   it "gets output" do
   	file_in = File.expand_path('../sample/sample_seq.fasta', File.dirname(__FILE__))
   	file_out = File.expand_path('../sample/sample_seq.afa', File.dirname(__FILE__))
-  	run = MuscleBio.run("sudo muscle -in #{file_in} -out #{file_out}")
+  	File.delete(file_out) if File.exist?(file_out)
+  	run = MuscleBio.run("sudo muscle -in #{file_in} -out #{file_out} -quiet")
   	expect( File.file?(file_out) ).to eq true
   end
 
