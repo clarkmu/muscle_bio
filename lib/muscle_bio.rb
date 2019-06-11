efea531f2011f7e33dd75eab1ff1726c2c62c475
Michael Clark <clarkmu@gmail.com>
2019-06-11 11:07:13 -0400

    check OS, add executables

--- lib/muscle_bio.rb
+++ lib/muscle_bio.rb
@@ -1,5 +1,26 @@
 require "muscle_bio/version"
 
 module MuscleBio
-  class Error < StandardError; end
+  def self.run(cmd)
+
+  	file = ""
+
+  	if (/darwin/ =~ RUBY_PLATFORM) != nil 
+  		file = "muscle_mac_64"
+  	elsif (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  		file = "muscle_windows_32.exe"
+  	else
+  		file = "muscle_linux_64"
+  	end
+
+  	file = File.join( File.dirname(__FILE__), 'runnable/'+file)
+
+  	cmd = cmd.sub 'sudo ', ''
+  	cmd = cmd.sub 'muscle', file
+  	# cmd = 'sudo ' + cmd
+
+  	# puts cmd
+
+	`#{cmd}`
+  end
 end
