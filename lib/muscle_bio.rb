88dd30977e9cbd09ab10a0d959e0e07e270f9050
Michael Clark <clarkmu@gmail.com>
2019-06-11 13:56:26 -0400

    test muscle output , check if -in file exists

--- lib/muscle_bio.rb
+++ lib/muscle_bio.rb
@@ -1,26 +1,34 @@
 require "muscle_bio/version"
 
 module MuscleBio
+
   def self.run(cmd)
-
-  	file = ""
-
-  	if (/darwin/ =~ RUBY_PLATFORM) != nil 
-  		file = "muscle_mac_64"
-  	elsif (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-  		file = "muscle_windows_32.exe"
-  	else
-  		file = "muscle_linux_64"
-  	end
-
+  	file = (/darwin/ =~ RUBY_PLATFORM) != nil ? "muscle_mac_64" :
+  		(/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil ? "muscle_windows_32.exe" : "muscle_linux_64"
   	file = File.join( File.dirname(__FILE__), 'runnable/'+file)
-
+  	if ! self.checkInFileExists(cmd)
+  		puts "-in DOES NOT EXIST"
+  		return
+  	end
   	cmd = cmd.sub 'sudo ', ''
   	cmd = cmd.sub 'muscle', file
-  	# cmd = 'sudo ' + cmd
-
-  	# puts cmd
-
+  	cmd = 'sudo ' + cmd
 	`#{cmd}`
   end
+
+  def self.checkInFileExists(cmd)
+  	fileIn = ''
+  	isInParam = false
+  	cmd.split(' ').each do |p|
+  		if isInParam
+  			fileIn = p
+  			isInParam = false
+  		end
+  		if p == "-in"
+  			isInParam = true
+  		end
+  	end
+  	return File.file?(fileIn)
+  end
+
 end
--- spec/muscle_bio_spec.rb
+++ spec/muscle_bio_spec.rb
@@ -1,9 +1,15 @@
 RSpec.describe MuscleBio do
-  it "has a version number" do
-    expect(MuscleBio::VERSION).not_to be nil
+
+  it "runs Muscle" do
+  	run = MuscleBio.run('muscle -version')
+  	expect(run).to eq "MUSCLE v3.8.31 by Robert C. Edgar\n"
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "gets output" do
+  	file_in = File.expand_path('../sample/sample_seq.fasta', File.dirname(__FILE__))
+  	file_out = File.expand_path('../sample/sample_seq.afa', File.dirname(__FILE__))
+  	run = MuscleBio.run("sudo muscle -in #{file_in} -out #{file_out}")
+  	expect( File.file?(file_out) ).to eq true
   end
+
 end
